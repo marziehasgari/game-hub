651ab004621d9b4100af34743e7d671cbf1b9cd2
Marzieh Asgari <ma.asgari@hamrah-mechanic.com>
2023-06-10 16:11:33 +0330

    display genres

--- src/App.tsx
+++ src/App.tsx
@@ -17,12 +17,16 @@ const App: React.FC = () => {
         base: `"nav" "main"`,
         lg: `"nav nav" "aside main"`,
       }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
     >
       <GridItem area="nav">
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside">
+        <GridItem area="aside" paddingX={5}>
           <GenreList />
         </GridItem>
       </Show>
