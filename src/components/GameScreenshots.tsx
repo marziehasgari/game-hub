31a9a2bf9afa056a8d341836d57e719392a1c725
Marzieh Asgari <ma.asgari@hamrah-mechanic.com>
2023-08-06 15:33:45 +0330

    update page title

--- src/components/GameScreenshots.tsx
+++ src/components/GameScreenshots.tsx
@@ -15,7 +15,7 @@ const GameScreenshots = ({ gameId }: Props) => {
   if (error) throw error;
 
   return (
-    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={2}>
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={2} marginTop={2}>
       {data?.results.map((file) => (
         <Image src={file.image} key={file.id} />
       ))}
